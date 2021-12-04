a9c85516883df03257722092658fa50e45cf9193
Chace Anderson <chace.anderson@slalom.com>
2021-12-04 18:09:01 -0500

    add integration test

--- src/test/java/com/app/todo/TodoApplicationTests.java
+++ src/test/java/com/app/todo/TodoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.app.todo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class TodoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mockMvc;
 
+	@Test
+	void testGetToDoListsByUsername() throws Exception {
+
+		mockMvc.perform(get("/list/{username}", "jsmith"))
+				.andExpect(status().isOk())
+				.andReturn();
+	}
 }
