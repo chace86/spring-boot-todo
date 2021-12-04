fed8383d96da0f4799e9f62989d5590a3e2bcbae
Chace Anderson <chace.anderson@slalom.com>
2021-12-04 18:30:41 -0500

    update test

--- src/test/java/com/app/todo/item/ToDoListItemServiceTests.java
+++ src/test/java/com/app/todo/item/ToDoListItemServiceTests.java
@@ -1,6 +1,7 @@
 package com.app.todo.item;
 
 import com.app.todo.list.ToDoList;
+import com.app.todo.list.ToDoListRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -22,74 +23,90 @@ public class ToDoListItemServiceTests {
     private static final ToDoListItem TODO_LIST_ITEM = new ToDoListItem(ID, TODO_LIST, "test code", false);
 
     @Mock
-    private ToDoListItemRepository repository;
+    private ToDoListItemRepository itemRepository;
+    @Mock
+    private ToDoListRepository listRepository;
 
     @InjectMocks
     private ToDoListItemService service;
 
     @Test
-    void testSaveToDoList() {
-        given(repository.existsById(ID)).willReturn(false);
+    void testSaveToDoListItem() {
+        given(itemRepository.existsById(ID)).willReturn(false);
+        given(listRepository.existsById(ID)).willReturn(true);
 
         assertTrue(service.saveToDoListItem(TODO_LIST_ITEM));
-        verify(repository, times(1)).existsById(ID);
-        verify(repository, times(1)).save(TODO_LIST_ITEM);
+        verify(itemRepository, times(1)).existsById(ID);
+        verify(listRepository, times(1)).existsById(ID);
+        verify(itemRepository, times(1)).save(TODO_LIST_ITEM);
     }
 
     @Test
-    void testSaveToDoListAlreadyExistsFalse() {
-        given(repository.existsById(ID)).willReturn(true);
+    void testSaveToDoListItemAlreadyExistsFalse() {
+        given(itemRepository.existsById(ID)).willReturn(true);
 
         assertFalse(service.saveToDoListItem(TODO_LIST_ITEM));
-        verify(repository, times(1)).existsById(ID);
-        verify(repository, times(0)).save(TODO_LIST_ITEM);
+        verify(itemRepository, times(1)).existsById(ID);
+        verify(listRepository, times(0)).existsById(ID);
+        verify(itemRepository, times(0)).save(TODO_LIST_ITEM);
     }
 
     @Test
-    void testUpdateToDoList() {
-        given(repository.findById(ID)).willReturn(Optional.of(TODO_LIST_ITEM));
+    void testSaveToDoListItemParentListDoesNotExist() {
+        given(itemRepository.existsById(ID)).willReturn(false);
+        given(listRepository.existsById(ID)).willReturn(false);
+
+        assertFalse(service.saveToDoListItem(TODO_LIST_ITEM));
+        verify(itemRepository, times(1)).existsById(ID);
+        verify(listRepository, times(1)).existsById(ID);
+        verify(itemRepository, times(0)).save(TODO_LIST_ITEM);
+    }
+
+    @Test
+    void testUpdateToDoListItem() {
+        given(itemRepository.findById(ID)).willReturn(Optional.of(TODO_LIST_ITEM));
         String description = "test description";
         ToDoListItem newTodoListItem =
                 new ToDoListItem(TODO_LIST_ITEM.getId(), TODO_LIST, description, true);
 
         assertTrue(service.updateToDoListItem(ID, description, true));
-        verify(repository, times(1)).findById(ID);
-        verify(repository, times(1)).save(newTodoListItem);
+        verify(itemRepository, times(1)).findById(ID);
+        verify(itemRepository, times(1)).save(newTodoListItem);
     }
 
     @Test
-    void testUpdateToDoListNotFound() {
-        given(repository.findById(ID)).willReturn(Optional.empty());
+    void testUpdateToDoListItemNotFound() {
+        given(itemRepository.findById(ID)).willReturn(Optional.empty());
         String description = "test description";
         ToDoListItem newTodoListItem =
                 new ToDoListItem(TODO_LIST_ITEM.getId(), TODO_LIST, description, true);
 
         assertFalse(service.updateToDoListItem(ID, description, true));
-        verify(repository, times(1)).findById(ID);
-        verify(repository, times(0)).save(newTodoListItem);
+        verify(itemRepository, times(1)).findById(ID);
+        verify(itemRepository, times(0)).save(newTodoListItem);
     }
 
     @Test
     void testFindToDoListItem() {
-        given(repository.findById(ID)).willReturn(Optional.of(TODO_LIST_ITEM));
+        given(itemRepository.findById(ID)).willReturn(Optional.of(TODO_LIST_ITEM));
 
         assertEquals(Optional.of(TODO_LIST_ITEM), service.findToDoListItem(ID));
-        verify(repository, times(1)).findById(ID);
+        verify(itemRepository, times(1)).findById(ID);
     }
 
     @Test
-    void testDeleteToDoList() {
-        given(repository.existsById(ID)).willReturn(true);
+    void testDeleteToDoListItem() {
+        given(itemRepository.existsById(ID)).willReturn(true);
 
         service.deleteToDoListItem(ID);
-        verify(repository, times(1)).deleteById(ID);
+        verify(itemRepository, times(1)).deleteById(ID);
     }
 
     @Test
-    void testDeleteToDoListNotFound() {
-        given(repository.existsById(ID)).willReturn(false);
+    void testDeleteToDoListItemNotFound() {
+        given(itemRepository.existsById(ID)).willReturn(false);
 
         service.deleteToDoListItem(ID);
-        verify(repository, times(0)).deleteById(ID);
+        verify(itemRepository, times(0)).deleteById(ID);
     }
 }
